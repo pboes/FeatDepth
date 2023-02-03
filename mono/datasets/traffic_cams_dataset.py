bb980017cea0b485bc9a5963203456bb1ba55a46
Paul Boes <paul.boes@porsche.digital>
2023-02-03 15:01:57 +0100

    debugging typos in dataset

--- mono/datasets/traffic_cams_dataset.py
+++ mono/datasets/traffic_cams_dataset.py
@@ -1,9 +1,11 @@
 from .mono_dataset import MonoDataset
+import os
+from PIL import Image  # using pillow-simd for increased speed
 
 
 class TrafficCamsDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
-        super(MonoDataset, self).__init__(*args, **kwargs)
+        super(TrafficCamsDataset, self).__init__(*args, **kwargs)
 
     def get_color(self, filename, do_flip):
         color = self.loader(os.path.join(self.data_path, filename))
