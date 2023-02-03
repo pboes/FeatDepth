068b676fb19cf7e696b46b70a2e269898ad01e12
Paul Boes <paul.boes@porsche.digital>
2023-02-03 14:56:57 +0100

    switching to custom dataset

--- mono/datasets/get_dataset.py
+++ mono/datasets/get_dataset.py
@@ -21,7 +21,7 @@ def get_dataset(cfg, training=True):
     elif dataset_name == "euroc":
         from .euroc_dataset import FolderDataset as dataset
     elif dataset_name == "traffic_cams":
-        from .mono_dataset import MonoDataset as dataset
+        from .traffic_cams_dataset import TrafficCamsDataset as dataset
 
     fpath = os.path.join(os.path.dirname(__file__), "splits", cfg.split, "{}_files.txt")
     filenames = (
--- mono/datasets/traffic_cams_dataset.py
+++ mono/datasets/traffic_cams_dataset.py
@@ -1,1 +1,14 @@
-from .mono_dataset import 
+from .mono_dataset import MonoDataset
+
+
+class TrafficCamsDataset(MonoDataset):
+    def __init__(self, *args, **kwargs):
+        super(MonoDataset, self).__init__(*args, **kwargs)
+
+    def get_color(self, filename, do_flip):
+        color = self.loader(os.path.join(self.data_path, filename))
+
+        if do_flip:
+            color = color.transpose(Image.FLIP_LEFT_RIGHT)
+
+        return color
