7f6e57c83d0e0a4fd6a74d75e590f3240eaa2b28
Paul Boes <paul.boes@porsche.digital>
2023-02-03 15:31:13 +0100

    writing custom methods for traffic cam dataset

--- mono/datasets/kitti_dataset.py
+++ mono/datasets/kitti_dataset.py
@@ -6,20 +6,25 @@ import numpy as np
 import PIL.Image as pil
 import datetime
 
-from .kitti_utils import generate_depth_map, read_calib_file, transform_from_rot_trans, pose_from_oxts_packet
+from .kitti_utils import (
+    generate_depth_map,
+    read_calib_file,
+    transform_from_rot_trans,
+    pose_from_oxts_packet,
+)
 from .mono_dataset import MonoDataset
 
 
 class KITTIDataset(MonoDataset):
-    """Superclass for different types of KITTI dataset loaders
-    """
+    """Superclass for different types of KITTI dataset loaders"""
+
     def __init__(self, *args, **kwargs):
         super(KITTIDataset, self).__init__(*args, **kwargs)
 
-        self.K = np.array([[0.58, 0, 0.5, 0],
-                           [0, 1.92, 0.5, 0],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]], dtype=np.float32)
+        self.K = np.array(
+            [[0.58, 0, 0.5, 0], [0, 1.92, 0.5, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
+            dtype=np.float32,
+        )
 
         self.full_res_shape = (1242, 375)
         self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
@@ -32,7 +37,8 @@ class KITTIDataset(MonoDataset):
         velo_filename = os.path.join(
             self.data_path,
             scene_name,
-            "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
+            "velodyne_points/data/{:010d}.bin".format(int(frame_index)),
+        )
 
         return os.path.isfile(velo_filename)
 
@@ -46,15 +52,16 @@ class KITTIDataset(MonoDataset):
 
 
 class KITTIRAWDataset(KITTIDataset):
-    """KITTI dataset which loads the original velodyne depth maps for ground truth
-    """
+    """KITTI dataset which loads the original velodyne depth maps for ground truth"""
+
     def __init__(self, *args, **kwargs):
         super(KITTIRAWDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(
-            self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
+            self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str
+        )
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
@@ -63,7 +70,8 @@ class KITTIRAWDataset(KITTIDataset):
         velo_filename = os.path.join(
             self.data_path,
             folder,
-            "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
+            "velodyne_points/data/{:010d}.bin".format(int(frame_index)),
+        )
 
         depth_gt = generate_depth_map(calib_path, velo_filename, self.side_map[side])
         depth_gt = scipy.misc.imresize(depth_gt, self.full_res_shape[::-1], "nearest")
@@ -74,37 +82,57 @@ class KITTIRAWDataset(KITTIDataset):
         return depth_gt
 
     def get_pose(self, folder, frame_index, offset):
-        oxts_root = os.path.join(self.data_path, folder, 'oxts')
-        with open(os.path.join(oxts_root, 'timestamps.txt')) as f:
-            timestamps = np.array([datetime.datetime.strptime(ts[:-3], "%Y-%m-%d %H:%M:%S.%f").timestamp()
-                                   for ts in f.read().splitlines()])
+        oxts_root = os.path.join(self.data_path, folder, "oxts")
+        with open(os.path.join(oxts_root, "timestamps.txt")) as f:
+            timestamps = np.array(
+                [
+                    datetime.datetime.strptime(
+                        ts[:-3], "%Y-%m-%d %H:%M:%S.%f"
+                    ).timestamp()
+                    for ts in f.read().splitlines()
+                ]
+            )
 
-        speed0 = np.genfromtxt(os.path.join(oxts_root, 'data', '{:010d}.txt'.format(frame_index)))[[8, 9, 10]]
+        speed0 = np.genfromtxt(
+            os.path.join(oxts_root, "data", "{:010d}.txt".format(frame_index))
+        )[[8, 9, 10]]
         # speed1 = np.genfromtxt(os.path.join(oxts_root, 'data', '{:010d}.txt'.format(frame_index+offset)))[[8, 9, 10]]
 
         timestamp0 = timestamps[frame_index]
-        timestamp1 = timestamps[frame_index+offset]
+        timestamp1 = timestamps[frame_index + offset]
         # displacement = 0.5 * (speed0 + speed1) * (timestamp1 - timestamp0)
         displacement = speed0 * (timestamp1 - timestamp0)
 
-        imu2velo = read_calib_file(os.path.join(self.data_path, os.path.dirname(folder), 'calib_imu_to_velo.txt'))
-        velo2cam = read_calib_file(os.path.join(self.data_path, os.path.dirname(folder), 'calib_velo_to_cam.txt'))
-        cam2cam = read_calib_file(os.path.join(self.data_path, os.path.dirname(folder), 'calib_cam_to_cam.txt'))
+        imu2velo = read_calib_file(
+            os.path.join(
+                self.data_path, os.path.dirname(folder), "calib_imu_to_velo.txt"
+            )
+        )
+        velo2cam = read_calib_file(
+            os.path.join(
+                self.data_path, os.path.dirname(folder), "calib_velo_to_cam.txt"
+            )
+        )
+        cam2cam = read_calib_file(
+            os.path.join(
+                self.data_path, os.path.dirname(folder), "calib_cam_to_cam.txt"
+            )
+        )
 
-        velo2cam_mat = transform_from_rot_trans(velo2cam['R'], velo2cam['T'])
-        imu2velo_mat = transform_from_rot_trans(imu2velo['R'], imu2velo['T'])
-        cam_2rect_mat = transform_from_rot_trans(cam2cam['R_rect_00'], np.zeros(3))
+        velo2cam_mat = transform_from_rot_trans(velo2cam["R"], velo2cam["T"])
+        imu2velo_mat = transform_from_rot_trans(imu2velo["R"], imu2velo["T"])
+        cam_2rect_mat = transform_from_rot_trans(cam2cam["R_rect_00"], np.zeros(3))
 
         imu2cam = cam_2rect_mat @ velo2cam_mat @ imu2velo_mat
 
-        odo_pose = imu2cam[:3,:3] @ displacement + imu2cam[:3,3]
+        odo_pose = imu2cam[:3, :3] @ displacement + imu2cam[:3, 3]
 
         return odo_pose
 
 
 class KITTIOdomDataset(KITTIDataset):
-    """KITTI dataset for odometry training and testing
-    """
+    """KITTI dataset for odometry training and testing"""
+
     def __init__(self, *args, **kwargs):
         super(KITTIOdomDataset, self).__init__(*args, **kwargs)
 
@@ -115,23 +143,22 @@ class KITTIOdomDataset(KITTIDataset):
             self.data_path,
             "sequences/{:02d}".format(int(folder)),
             "image_{}".format(side_map[side]),
-            f_str)
+            f_str,
+        )
         return image_path
 
 
 class KITTIDepthDataset(KITTIDataset):
-    """KITTI dataset which uses the updated ground truth depth maps
-    """
+    """KITTI dataset which uses the updated ground truth depth maps"""
+
     def __init__(self, *args, **kwargs):
         super(KITTIDepthDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(
-            self.data_path,
-            folder,
-            "image_0{}/data".format(self.side_map[side]),
-            f_str)
+            self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str
+        )
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
@@ -140,7 +167,8 @@ class KITTIDepthDataset(KITTIDataset):
             self.data_path,
             folder,
             "proj_depth/groundtruth/image_0{}".format(self.side_map[side]),
-            f_str)
+            f_str,
+        )
 
         depth_gt = pil.open(depth_path)
         depth_gt = depth_gt.resize(self.full_res_shape, pil.NEAREST)
--- mono/datasets/traffic_cams_dataset.py
+++ mono/datasets/traffic_cams_dataset.py
@@ -1,16 +1,88 @@
+import random
 from .mono_dataset import MonoDataset
 import os
-from PIL import Image  # using pillow-simd for increased speed
+import PIL.Image as pil  # using pillow-simd for increased speed
+
+import torch
+from torchvision import transforms
 
 
 class TrafficCamsDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
         super(TrafficCamsDataset, self).__init__(*args, **kwargs)
 
-    def get_color(self, filename, do_flip):
-        color = self.loader(os.path.join(self.data_path, filename))
+    def __getitem__(self, index):
+        """Returns a single training item from the dataset as a dictionary.
+
+        Values correspond to torch tensors.
+        Keys in the dictionary are either strings or tuples:
+
+            ("color", <frame_id>, <scale>)          for raw colour images,
+            ("color_aug", <frame_id>, <scale>)      for augmented colour images,
+            ("K", scale) or ("inv_K", scale)        for camera intrinsics,
+            "stereo_T"                              for camera extrinsics, and
+            "depth_gt"                              for ground truth depth maps.
+
+        <frame_id> is either:
+            an integer (e.g. 0, -1, or 1) representing the temporal step relative to 'index',
+        or
+            "s" for the opposite image in the stereo pair.
+
+        <scale> is an integer representing the scale of the image relative to the fullsize image:
+            -1      images at native resolution as loaded from disk
+            0       images resized to (self.width,      self.height     )
+            1       images resized to (self.width // 2, self.height // 2)
+            2       images resized to (self.width // 4, self.height // 4)
+            3       images resized to (self.width // 8, self.height // 8)
+        """
+        inputs = {}
+
+        do_color_aug = self.is_train and random.random() > 0.5
+        do_flip = self.is_train and random.random() > 0.5
+
+        if not self.is_train and self.gt_depth_path is not None:
+            gt_depth = self.gt_depths[index]
+            inputs["gt_depth"] = gt_depth
+
+        for i in self.frame_idxs:
+            inputs[("color", i, -1)] = self.get_color(index, i, do_flip)
+
+        # adjusting intrinsics to match each scale in the pyramid
+        K = self.K.copy()
+        K[0, :] *= self.width
+        K[1, :] *= self.height
+        inv_K = np.linalg.pinv(K)
+
+        inputs[("K")] = torch.from_numpy(K)
+        inputs[("inv_K")] = torch.from_numpy(inv_K)
+
+        if do_color_aug:
+            color_aug = transforms.ColorJitter(
+                brightness=self.brightness,
+                contrast=self.contrast,
+                saturation=self.saturation,
+                hue=self.hue,
+            )
+        else:
+            color_aug = lambda x: x
+
+        self.preprocess(inputs, color_aug)
+
+        for i in self.frame_idxs:
+            del inputs[("color", i, -1)]
+        return inputs
+
+    def get_color(self, frame_index, relative_index, do_flip):
+        if (frame_index == 0) and (relative_index <= 0):
+            other_index = 0
+        elif (frame_index == len(self.filenames)) and (relative_index >= 0):
+            other_index = len(self.filenames)
+        else:
+            other_index = frame_index + relative_index
+
+        color = self.loader(self.filenames[other_index])
 
         if do_flip:
-            color = color.transpose(Image.FLIP_LEFT_RIGHT)
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
 
         return color
